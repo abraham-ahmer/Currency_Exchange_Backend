4039b25539286c7e33b12981dd912a0b0f38dce8
Abraham Ahmer <utdahmer@gmail.com>
2026-04-26 15:17:41 +0500

    tuning in pool agressively to avoid extra time for getting response

--- database.py
+++ database.py
@@ -9,7 +9,14 @@ db_url = os.getenv("DATABASE_URL")
 if not db_url:
     raise RuntimeError("DATABASE_URL is not set.")
 
-engine = create_engine(db_url, pool_pre_ping=True, pool_recycle=300) # Checks if connection is alive before using it and refreshes it every 5 mnt.
+engine = create_engine(
+    db_url,
+    pool_pre_ping=True,   # checks if connection is alive before using it
+    pool_recycle=300,     # recycles connections every 5 minutes
+    pool_size=5,          # number of persistent connections
+    max_overflow=10,      # extra connections allowed beyond pool_size
+    pool_timeout=30       # wait time before giving up on a connection
+)
 
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
